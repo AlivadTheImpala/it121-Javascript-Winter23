825e339ed87cb19d342000fa06a23dad44d86054
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-01-09 16:45:03 -0800

    added week1 assignment

---
+++ week1/script.js
@@ -0,0 +1,5 @@
+const firstName = "Brandon";
+const lastName = "Davila";
+let yearsOfStudy = 2;
+let goal =
+  "help my dad build a better website and earn more money to allow me to explore my hobbies more deeply";
